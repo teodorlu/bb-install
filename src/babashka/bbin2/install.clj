5053a916625ba3d4cc09ffa8f44e849d0b759132
Teodor Heggelund <git@teod.eu>
2024-06-18 12:47:24 +0200

    bbin2 install: embed subcommand helptext

--- src/babashka/bbin2/install.clj
+++ src/babashka/bbin2/install.clj
@@ -3,7 +3,8 @@
    [babashka.bbin.dirs :as bbin1.dirs]
    [babashka.bbin.protocols :as bbin1.protocols]
    [babashka.bbin.scripts :as bbin1.scripts]
-   [babashka.bbin.util :as bbin1.util]))
+   [babashka.bbin.util :as bbin1.util]
+   [clojure.string :as str]))
 
 (defn install [cli-opts]
   (bbin1.dirs/ensure-bbin-dirs cli-opts)
@@ -56,9 +57,59 @@
 
         #_ ""))))
 
+(def install-helptext
+  (str/trim "
+bbin install [script]
+
+- By default, scripts will be installed to `~/.local/bin`
+    - If `$BABASHKA_BBIN_BIN_DIR` is set, then use `$BABASHKA_BBIN_BIN_DIR` (explicit override)
+- Each bin script is a self-contained shell script that fetches deps and invokes `bb` with the correct arguments.
+- The bin scripts can be configured using the CLI options or the `:bbin/bin` key in `bb.edn`
+- [See the FAQ for additional info on setting up your code to work with bbin](docs/faq.md#how-do-i-get-my-software-onto-bbin)
+
+**Example `bb.edn` Config:**
+
+```clojure
+{:bbin/bin {neil {:main-opts [\"-f\" \"neil\"]}}}
+```
+
+**Supported Options:**
+
+*Note:* `bbin` will throw an error if conflicting options are provided, such as using both `--git/sha` and `--mvn/version` at the same time.
+
+If no `--git/tag` or `--git/sha` is provided, the latest tag from the Git repo will be used. If no tags exist, the latest SHA will be used.
+
+- `--as`
+    - The name of the script to be saved in the `bbin bin` path
+- `--git/sha`
+    - The SHA for a Git repo
+- `--git/tag`
+    - The tag for a Git repo
+- `--git/url`
+    - The URL for a Git repo
+- `--latest-sha`
+    - If provided, find the latest SHA from the Git repo
+- `--local/root`
+    - The path of a local directory containing a `deps.edn` file
+- `--main-opts`
+    - The provided options (EDN format) will be passed to the `bb` command-line when the installed script is run
+    - By default, `--main-opts` will be set to `[\"-m\" ...]`, inferring the main function from the lib name
+    - For example, if you provide a lib name like `io.github.rads/watch`, `bbin` will infer `rads.watch/-main`
+    - Project authors can provide a default in the `:bbin/bin` key in `bb.edn`
+- `--mvn/version`
+    - The version of a Maven dependency
+- `--ns-default`
+    - The namespace to use to find functions (tool mode only)
+    - Project authors can provide a default in the `:bbin/bin` key in `bb.edn`
+- `--tool`
+    - If this option is provided, the script will be installed using **tool mode**
+    - When enabled, the installed script acts as an entry point for functions in a namespace, similar to `clj -T`
+    - If no function is provided, the installed script will infer a help message based on the function docstrings
+"))
+
 (defn cmd-install [args+opts]
   (if-let [opts (parse-opts args+opts)]
     (install opts)
     (do
-      (bbin1.util/print-help)
+      (println install-helptext)
       (System/exit 1))))
