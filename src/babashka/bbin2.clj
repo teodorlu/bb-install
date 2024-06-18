58f90ea9a57b1cd16fcd30a800f6cab70fac7133
Teodor Heggelund <git@teod.eu>
2024-06-18 12:14:05 +0200

    bbin2: display subcommands

--- src/babashka/bbin2.clj
+++ src/babashka/bbin2.clj
@@ -1,10 +1,21 @@
 (ns babashka.bbin2
   (:require
    [babashka.cli :as cli]
-   [babashka.bbin2.install]))
+   [babashka.bbin2.install]
+   [clojure.string :as str]))
+
+(declare dispatch-table)
+
+(defn help [_]
+  (println "Available subcommands:")
+  (println)
+  (doseq [row dispatch-table]
+    (when (seq (:cmds row))
+      (println "  " (str/join " " (:cmds row))))))
 
 (def dispatch-table
-  [{:cmds ["install"] :fn babashka.bbin2.install/install}])
+  [{:cmds ["install"] :fn babashka.bbin2.install/install}
+   {:cmds []          :fn help}])
 
 (defn -main [& args]
   (binding [*print-namespace-maps* false]
