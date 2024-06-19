ea756fcb4888e20743311f0446cf64ea4776e234
Teodor Heggelund <git@teod.eu>
2024-06-19 12:26:41 +0200

    More drafting, configuration namespace

--- src/babashka/bbin/configuration.clj
+++ src/babashka/bbin/configuration.clj
@@ -12,4 +12,39 @@
   The whole configuration is not known immediately: we know CLI arguments and
   environment variables when bbin starts, but certain configuration depends on
   the installation target, bbin script authors can provide certain configuration
-  in `deps.edn` files.")
+  in `deps.edn` files."
+  (:require [babashka.process :as process]))
+
+(defn infer-is-tty? []
+  (let [fd 1 key :out]
+    (-> ["test" "-t" (str fd)]
+        (process/process {key :inherit :env {}})
+        deref
+        :exit
+        (= 0))))
+
+(defn is-tty [conf]
+  (get conf :bbin.configuration/is-tty))
+
+(comment
+  ;; incomplete, a draft
+
+  (defn create-configuration [cli-opts environment is-tty])
+
+  ;; the idea is to let the toplevel main create a configuration by providing
+  ;; the side-effecting stuff on the top level, ie
+
+  (fn [cli-opts]
+    (let [configuration (create-configuration cli-opts (System/getenv) (infer-is-tty?))]
+      ,,,,))
+
+  ;; then use accessor functions to pull things out,
+
+  (require '[babashka.bbin.configuration :as configuration])
+  (fn [cli-opts]
+    (let [conf (create-configuration cli-opts (System/getenv) (infer-is-tty?))]
+      (configuration/is-tty conf)
+      ,,,,))
+
+
+  )
